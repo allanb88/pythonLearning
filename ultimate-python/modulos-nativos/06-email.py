2630765c896ce0efdcb57bf4ae7f227d6facdf59
Allan B. <allanbs.88@gmail.com>
2025-08-05 15:08:13 -0600

    agregando ultimos cambios

--- ultimate-python/modulos-nativos/06-email.py
+++ ultimate-python/modulos-nativos/06-email.py
@@ -4,11 +4,11 @@ import smtplib
 from email.mime.image import MIMEImage
 from pathlib import Path
 
-path = Path("modulos-nativos/holamundo.png")
+path = Path("py.jpg")
 mime_image = MIMEImage(path.read_bytes())
 mensaje = MIMEMultipart()
 mensaje["from"] = "Hola Mundo"
-mensaje["to"] = "ultimatepython@holamundo.io"
+mensaje["to"] = "allanbs.88@gmail.com"
 mensaje["subject"] = "Esta es una prueba"
 cuerpo = MIMEText("Cuerpo del mensaje")
 mensaje.attach(cuerpo)
@@ -18,6 +18,6 @@ with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
 
-    smtp.login("ultimatepython@holamundo.io", "holamundo123")
+    smtp.login("allanbs.88@gmail.com", "hggpbbquxapnlseh")
     smtp.send_message(mensaje)
     print("Mensaje enviado")
